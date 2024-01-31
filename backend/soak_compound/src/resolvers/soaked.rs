5d25ce4e7e66c458e1157f4048c8d1b09ea8e047
iamvigneshwars <iamvigneshwars@gmail.com>
2024-01-31 16:44:32 +0000

    query soaked compounds optimized

--- backend/soak_compound/src/resolvers/soaked.rs
+++ backend/soak_compound/src/resolvers/soaked.rs
@@ -1,15 +1,11 @@
 // src/resolvers/soaked.rs
 
-use crate::entities::{compounds, soaked, wells};
+use std::collections::HashMap;
 use async_graphql::{ComplexObject, Context, Object, SimpleObject};
 use sea_orm::{prelude::*, ActiveValue};
 use opa_client::subject_authorization;
+use crate::entities::{compounds, soaked, wells};
 
-#[derive(SimpleObject)]
-pub struct CompoundWithVolume {
-    compound: compounds::Model,
-    volume: f64,
-}
 
 #[derive(Debug, Clone, Default)]
 pub struct SoakedQuery;
@@ -33,19 +29,27 @@ impl soaked::Model {
     }
 }
 
+#[derive(SimpleObject)]
+pub struct CompoundWithVolume {
+    compounds: compounds::Model,
+    volume: f64,
+}
+
 #[Object]
 impl SoakedQuery {
+
     async fn soaked(&self, ctx: &Context<'_>) -> async_graphql::Result<Vec<soaked::Model>> {
         subject_authorization!("xchemlab.soak_compound.read_soaked", ctx).await?;
         let db = ctx.data::<DatabaseConnection>().unwrap();
         Ok(soaked::Entity::find().all(db).await?)
     }
-
+    
     async fn compounds_in_well(
         &self,
         ctx: &Context<'_>,
         well_id: i32,
     ) -> async_graphql::Result<Vec<CompoundWithVolume>> {
+
         subject_authorization!("xchemlab.soak_compound.read_soaked", ctx).await?;
         let db = ctx.data::<DatabaseConnection>().unwrap();
         let soaked_wells = soaked::Entity::find()
@@ -53,20 +57,43 @@ impl SoakedQuery {
             .all(db)
             .await?;
 
-        let mut soaked_compounds: Vec<CompoundWithVolume> = Vec::new();
-        for well in soaked_wells {
-            let compound = compounds::Entity::find_by_id(well.compound_id)
-                .one(db)
-                .await?
-                .ok_or(DbErr::RecordNotFound(format!(
-                    "Compound not found with id {} ",
-                    well.compound_id
-                )))?;
-            soaked_compounds.push(CompoundWithVolume {
-                compound,
+        let compound_ids: Vec<i32> = soaked_wells
+            .iter()
+            .map(|well| well.compound_id)
+            .collect();
+
+        let compound_entities = compounds::Entity::find()
+            .filter(compounds::Column::Id.is_in(compound_ids))
+            .all(db)
+            .await?
+            .into_iter()
+            .map(|compound| (compound.id, compound))
+            .collect::<HashMap<i32, compounds::Model>>();
+
+        
+        let soaked_compounds: Vec<CompoundWithVolume> = soaked_wells.into_iter().filter_map(|well| {
+            let compound = compound_entities.get(&well.compound_id)?;
+            Some(CompoundWithVolume{
+                compounds: compound.clone(),
                 volume: well.volume,
-            });
-        }
+            })
+        }).collect();
+
+
+        // let mut soaked_compounds: Vec<CompoundWithVolume> = Vec::new();
+        // for well in soaked_wells {
+        //     let compound = compounds::Entity::find_by_id(well.compound_id)
+        //         .one(db)
+        //         .await?
+        //         .ok_or(DbErr::RecordNotFound(format!(
+        //             "Compound not found with id {} ",
+        //             well.compound_id
+        //         )))?;
+        //     soaked_compounds.push(CompoundWithVolume {
+        //         compounds: compound,
+        //         volume: well.volume,
+        //     });
+        // }
 
         Ok(soaked_compounds)
     }
